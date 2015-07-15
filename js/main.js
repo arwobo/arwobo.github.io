779179284891031b4cdb7fbe8ef31b9c3efa305c
arwobo <ariel.bolles@gmail.com>
2015-07-14 22:59:54 -0500

    Updates
    Working Contact area and kind of working scroll

--- js/main.js
+++ js/main.js
@@ -2,26 +2,86 @@
 
 	$(window).scroll(function() {
 		
+		// if the windowis more than 500px from top,
 		if ($(this).scrollTop() > 500){  
    		
+   		// add sticky class to header
    		$('header').addClass("sticky");
 
+   		// hide h2
    		$('h2').slideUp();
 	}
-	else{
+	
+	// otherwise
+	else {
 		
+		// remove sticky class
 		$('header').removeClass("sticky");
 
+		// show h2S
 		$('h2').slideDown();
 	}
 
 	});
 
-// Scroll to page sections interaction
+// Scroll to page sections interaction on click
 
-	// On click of a nav link
+	// store nav height
+		var navHeight = $("nav").outerHeight();
 
-	// Scroll to the associated page section (leaving room for sticky nav above)
+		// On click of a nav link
+		$("nav a").on("click", function (event) {
+			// prevent standard link operation	
+			event.preventDefault();
+
+			// Remove active class from all nav links
+			$("nav a").removeClass("active");
+
+			// Give active class to nav link
+			$(this).addClass("active");
+
+			// use href to identify which page section to go to
+			var sectionTarget = $(this).attr("href");
+
+			// find the top of the section
+			var targetOffset = $(sectionTarget).offset().top - navHeight;
+
+			// animate it
+			$("body").animate({
+				scrollTop: targetOffset
+			}, 600);
+
+		});	
+
+// Highlighting nav areas on scroll
+
+	// Create variables for sections, nav, and nav height
+	var sections = $('section');
+	var nav = $('nav');
+	var nav_height = nav.outerHeight();
+	 
+	// On scroll, determine how far window position is from top of page
+	$(window).on('scroll', function () {
+	  var cur_pos = $(this).scrollTop();
+	 
+	  // for each section,  
+	  sections.each(function() {
+	    // define variables for top and bottom of section
+	    var top = $(this).offset().top - nav_height;
+	    var bottom = top + $(this).outerHeight();
+	 
+	    // if the current position is greater than top value, and less than bottom,
+	    if (cur_pos >= top && cur_pos <= bottom) {
+	      // remove active classes from nav links
+	      nav.find('a').removeClass('active');
+	      sections.removeClass('active');
+	 
+	      // add active class to section link
+	      $(this).addClass('active');
+	      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    }
+	  });
+	});
 
 
 // Contact area interaction 
@@ -31,21 +91,23 @@
 		event.preventDefault();
 		
 		// Hide "contact" image
-		$("#contactImageLink").slideUp();
+		$(".contactImageLink").removeClass("show");
 
 		// Contact info slides down
-		$("#contactInfo").slideDown().css("display");
+		$(".contactInfo").addClass("show");
 	});		
 
 	// When "X" link is selected 
 	$("#contactInfoClose").on("click", function (event) {
 		event.stopPropagation();
 
+		event.preventDefault();	
+
 		// Slide up contact info
-		$("#contactInfo").slideUp().css("hide");
+		$(".contactInfo").removeClass("show");
 
 		//Show "contact" image 
-		$("#contactImageLink").slideDown();
+		$(".contactImageLink").addClass("show");
 	});	
 							
 
