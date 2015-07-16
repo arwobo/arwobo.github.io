ca79d7dea8743ccfe8a1a0605d1251a665874c59
arwobo <ariel.bolles@gmail.com>
2015-07-15 21:10:37 -0500

    New updates
    Video changes, nav works

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 	$(window).scroll(function() {
 		
 		// if the windowis more than 500px from top,
-		if ($(this).scrollTop() > 500){  
+		if ($(this).scrollTop() > $('.quote').offset().top - 600){  
    		
    		// add sticky class to header
    		$('header').addClass("sticky");
@@ -26,9 +26,9 @@
 
 // Scroll to page sections interaction on click
 
-	// store nav height
-		var navHeight = $("nav").outerHeight();
-
+	// var navHeight2 = $("nav").outerHeight();
+	// console.log(navHeight2);
+	
 		// On click of a nav link
 		$("nav a").on("click", function (event) {
 			// prevent standard link operation	
@@ -37,21 +37,26 @@
 			// Remove active class from all nav links
 			$("nav a").removeClass("active");
 
-			// Give active class to nav link
+			// Give active class to selected nav link
 			$(this).addClass("active");
 
 			// use href to identify which page section to go to
 			var sectionTarget = $(this).attr("href");
 
 			// find the top of the section
-			var targetOffset = $(sectionTarget).offset().top - navHeight;
+			var targetOffset = $(sectionTarget).offset().top - 50;
 
 			// animate it
-			$("body").animate({
-				scrollTop: targetOffset
+			$("html, body").animate({scrollTop: targetOffset
 			}, 600);
 
 		});	
+
+// Clear "active" nav class on click of H1 a
+
+	$("h1 a").on("click", function () {
+		$("nav a").removeClass("active");
+	})		
 
 // Highlighting nav areas on scroll
 
@@ -112,87 +117,9 @@
 							
 
 
-// // In "Media" section (video slider interaction)
+// // In "Listen" section (video slider interaction)
 	
-// 	// For every slider (in case there is ever another slider added to this page)
-// 	$(".slideViewer").each(function () {
-// 		// Get current slider
-// 		var $this = $(this);
-// 		// Get the slide-group (container)
-// 		var $group = $this.find("slide.group");
-// 		// JQuery object to hold all slides
-// 		var $slides = $this.find(".slide");
-// 		// Create array to hold nav buttons
-// 		var buttonArray = [];
-// 		// Index number of current slide
-// 		var currentIndex = 0;
 
-// 		// move() - function for moving slides
-// 		// Creates the slide from old one to new one
-// 		function move(newIndex) {
-// 			// declare variables
-// 			var animateLeft, slideLeft;
-
-// 			// when slide moves, call advance again
-// 			advance();
-
-// 			// if current slide is showing or slide is animating, do nothing
-// 			if ($group.is(":animated") || currentIndex === newIndex) {
-// 				return;
-// 			}
-
-// 			// remove class from item
-// 			buttonArray[currentIndex].removeClass("active");
-// 			// add class to new item
-// 			buttonArray[newIndex].addClass("active");
-
-// 			// if new item > current
-// 			if (newIndex > currentIndex) {
-// 				// Sit the new slide to the right
-// 				slideLeft = "100%";
-// 				// Animate the current group to the left
-// 				animateLeft = "-100%";
-// 			} else {
-// 				// Sit the new slide to the Left
-// 				slideLeft = "-100%";
-// 				// Animate the current group to the right
-// 				animateLeft = "100%";
-// 			}
-// 			// Position new slide to left (if less) or right (if more) of current
-// 			$slides.eq(newIndex).css( {left: slideLeft, display: "block"} );
-// 			// Animate slides and
-// 			$group.animate( {left: animateLeft} , function () {
-// 				// Hide previous slide
-// 				$slides.eq(currentIndex).css( {display: "none"} );
-// 				// Set position of the new item
-// 				$slides.eq(newIndex).css( {left: 0} );
-// 				// Set position of group of slides
-// 				$group.css( {left: 0} );
-// 				// Set currentIndex to new video
-// 				currentIndex = newIndex;
-// 			});
-
-// 		}
-
-// 		$.each($slides, function(index) {
-// 			// Create a button element for the button
-// 			var $button = $("<button type="button" class="slide-btn">&bull;</button>");
-// 			// If index is the current item
-// 			if (index === currentIndex) {
-// 				// Add the active class
-// 				$button.addClass("active");
-// 			}
-// 			// Create event handler for the button
-// 			$button.on("click", function () {
-// 				// call the "move" function
-// 				move(index);
-// 				// Add to the buttons holder
-// 			}).appendTo(".slide-buttons");
-// 			// Add it to the button array
-// 			buttonArray.push($button);
-// 		});
-
-// 	});
 
 
 
